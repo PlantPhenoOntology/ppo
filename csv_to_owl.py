4619b3fae2f6768a4c77300d3b37067b1bca59ed
stuckyb <stuckyb@flmnh.ufl.edu>
2016-08-30 15:42:32 -0400

    Added code documentation and did some minor refactoring.

--- csv_to_owl.py
+++ csv_to_owl.py
@@ -52,6 +52,12 @@ for termsfile in args.termsfiles:
         )
 
 def makeLabelMap(ontology):
+    """
+    Constructs a dictionary for a given ontology that maps class labels (i.e.,
+    the values of rdfs:label axioms) to their corresponding class IRIs.  This
+    function verifies that none of the labels are ambiguous; that is, that no
+    label is used for more than one IRI.
+    """
     # Create a dictionary that maps term labels to their IRIs.
     labelmap = {}
     for annotation_axiom in ontology.getAxioms(AxiomType.ANNOTATION_ASSERTION, True):
@@ -72,15 +78,24 @@ def makeLabelMap(ontology):
 
     return labelmap
 
-def getParentIRIFromTable(tdata, rowcnt):
-    tdata = tdata.strip()
+def getParentIRIFromCSVRow(csvrow, rowcnt, labelmap):
+    """
+    Parses a superclass (parent) IRI from a single row of an input CSV file.
+    The parent class information should be in a column called "Parent class".
+    Either a class label, ID, or both can be provided.  The general format is:
+    "'class label' (CLASS_ID)".  For example: "'whole plant' (PO:0000003)".  If
+    both a label and ID are provided, this function will verify that they
+    match by looking up the label in a dictionary mapping labels to class IRIs,
+    which should be provided as the third argument.
+    """
+    tdata = csvrow['Parent class'].strip()
     if tdata == '':
-        raise Exception('No parent class was provided in row ' + str(rowcnt) + '.')
+        raise RuntimeError('No parent class was provided in row ' + str(rowcnt) + '.')
 
     # Check if we have a class label.
     if tdata.startswith("'"):
         if tdata.find("'") == tdata.rfind("'"):
-            raise Exception('Missing closing quote in parent class specification in row '
+            raise RuntimeError('Missing closing quote in parent class specification in row '
                         + str(rowcnt) + ': "' + tdata + '".')
         label = tdata.split("'")[1]
         labelIRI = labelmap[label]
@@ -92,7 +107,7 @@ def getParentIRIFromTable(tdata, rowcnt):
                 tdID = tdID.rstrip(')')
                 tdIRI = IRI.create(OBO_BASE_IRI + tdID.replace(':', '_'))
             else:
-                raise Exception('Missing closing parenthesis in parent class specification in row '
+                raise RuntimeError('Missing closing parenthesis in parent class specification in row '
                         + str(rowcnt) + ': "' + tdata + '".')
     else:
         # We only have an ID.
@@ -104,20 +119,25 @@ def getParentIRIFromTable(tdata, rowcnt):
             if labelIRI.equals(tdIRI):
                 return labelIRI
             else:
-                raise Exception('Class label does not match ID in parent class specification in row '
+                raise RuntimeError('Class label does not match ID in parent class specification in row '
                         + str(rowcnt) + ': "' + tdata + '".')
         else:
             return labelIRI
     else:
         return tdIRI
 
-def getAnnotations(csvrow, rowcnt):
+def getAnnotationsFromCSVRow(csvrow, rowcnt):
+    """
+    Processes annotation columns in a single row of an input CSV file.
+    Currently, only label and definition annotations are supported.
+    The results are returned as a list of OWLAnnotation objects.
+    """
     annotations = []
 
     # Make sure we have a label and add it to the new class.
     labeltext = csvrow['Label'].strip()
     if labeltext == '':
-        raise Exception('No label was provided for ' + csvrow['ID']
+        raise RuntimeError('No label was provided for ' + csvrow['ID']
                 + ' (row ' + str(rowcnt) + ').')
     labelannot = df.getOWLAnnotation(
         df.getRDFSLabel(), df.getOWLLiteral(csvrow['Label'], 'en')
@@ -135,7 +155,11 @@ def getAnnotations(csvrow, rowcnt):
 
     return annotations
 
-def processCSVRow(csvrow, rowcnt):
+def processCSVRow(csvrow, rowcnt, labelmap):
+    """
+    Processes a single row in a source CSV file, where the row defines a new
+    class to add to the target ontology.
+    """
     # Create the new class.
     classIRI = IRI.create(OBO_BASE_IRI + csvrow['ID'].replace(':', '_'))
     newclass = df.getOWLClass(classIRI)
@@ -143,7 +167,7 @@ def processCSVRow(csvrow, rowcnt):
     ontman.applyChange(AddAxiom(base_ontology, declaxiom))
     
     # Add the annotations.
-    annotations = getAnnotations(csvrow, rowcnt)
+    annotations = getAnnotationsFromCSVRow(csvrow, rowcnt)
     for annotation in annotations:
         annotaxiom = df.getOWLAnnotationAssertionAxiom(classIRI, annotation)
         ontman.applyChange(AddAxiom(base_ontology, annotaxiom))
@@ -153,12 +177,12 @@ def processCSVRow(csvrow, rowcnt):
     
     # Get the OWLClass object of the parent class, making sure that it is
     # actually defined.
-    parentIRI = getParentIRIFromTable(csvrow['Parent class'], rowcnt)
+    parentIRI = getParentIRIFromCSVRow(csvrow, rowcnt, labelmap)
     parentclass = df.getOWLClass(parentIRI)
     # The method below of checking for class declaration does not work for
     # classes from imports.  TODO: Find another way to do this.
     #if (base_ontology.getDeclarationAxioms(parentclass).size() == 0):
-    #    raise Exception('The parent class for ' + csvrow['ID'] + ' (row '
+    #    raise RuntimeError('The parent class for ' + csvrow['ID'] + ' (row '
     #            + str(rowcnt) + ') could not be found.')
     
     # Add the subclass axiom to the ontology.
@@ -184,8 +208,9 @@ labelmap = makeLabelMap(base_ontology)
 
 OBO_BASE_IRI = 'http://purl.obolibrary.org/obo/'
 DEFINITION_IRI = IRI.create(OBO_BASE_IRI + 'IAO_0000115')
-df = OWLManager.getOWLDataFactory()
 
+# Create an OWL data factory and Manchester Syntax parser.
+df = OWLManager.getOWLDataFactory()
 mparser = ManchesterSyntaxTool(base_ontology)
 
 for termsfile in args.termsfiles:
@@ -195,7 +220,7 @@ for termsfile in args.termsfiles:
         for line in reader:
             rowcnt += 1
             if not(line['Ignore'].strip().startswith('Y')):
-                processCSVRow(line, rowcnt)
+                processCSVRow(line, rowcnt, labelmap)
             
 mparser.dispose()
 
